f93c746713c65fcae214f85906a6cced8d97c74d
JoshInkiriwang <63942453+JoshInkiriwang@users.noreply.github.com>
2023-10-12 12:21:16 +0700

    Code Correction (variables)

--- eMG100_Calculator/eMG100.py
+++ eMG100_Calculator/eMG100.py
@@ -7,7 +7,5 @@ def main():
     print(f"Jumlah Card SLIB8 : {count_SLIB8}")
     print(f"Jumlah Card SLIU8 : {count_SLIU8}")
 
-
-
 if __name__ == "__main__" :
     main()
--- eMG100_Calculator/hitung_SLT.py
+++ eMG100_Calculator/hitung_SLT.py
@@ -2,17 +2,17 @@ from constant_eMG100 import MAX_CAPACITY_SLT_CARD
 
 def itungan_SLT(default, min_SLIB8, min_SLIU8, max_SLIB8, max_SLIU8):
     counter = 0
-    SLIB8_card = 0
-    SLIU8_card = 0
+    SLIB8_card = min_SLIB8
+    SLIU8_card = min_SLIU8
 
-    input_SLT = int(input("Berapa Telepon Single ? "))
+    input_SLT = int(input("Berapa Telepon Single Line ? "))
     
     # 0 tersebut berarti jumlah terkecil untuk Single Line Telephone
     if 0 <= input_SLT <= default :
         return 0 # Tidak perlu card tambahan untuk Single Line Telephone
     else : 
         lebih = input_SLT - default
-        while(lebih >= MAX_CAPACITY_SLT_CARD and SLIU8_card <= 2) :
+        while(lebih >= MAX_CAPACITY_SLT_CARD and SLIU8_card <= max_SLIB8) :
             if counter % 2 == 0: 
                 lebih -= MAX_CAPACITY_SLT_CARD
                 SLIB8_card += 1
