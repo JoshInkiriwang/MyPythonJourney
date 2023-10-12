5532a799debf1411fee9a1e43d804fdb7764f752
JoshInkiriwang <63942453+JoshInkiriwang@users.noreply.github.com>
2023-10-12 14:26:26 +0700

    Add some constants

--- eMG100_Calculator/constant_eMG100.py
+++ eMG100_Calculator/constant_eMG100.py
@@ -6,9 +6,17 @@ KSUS_DEFAULT_SLT = 6 # Default Single Line Telephone Slot for PBX
 KSUS_DEFAULT_DISPLAY = 2 # Default Digital Phone Slot for PBX
 KSUS_DEFAULT_CO = 0 # Default CO Line Slot for PBX
 
-# Section Card untuk Single Line Telephone
+# Section Card untuk Digital Telephone (Universal Slot)
+MAX_DTIB = 2 # This is a main card for Display Key Telephone, 
+# DTIB have a daughter slot for COIU2/COIU4/SLIU8
+MAX_CAPACITY_DISPLAY_CARD = 8 # This is a capacity for DTIB8
+
+# Section Card untuk Single Line Telephone (Universal Slot)
 MIN_SLIB8 = 0 # This is a main card for Single Line Telephone
 MIN_SLIU8 = 0 # SLIU8 can attach on SLIB8
 MAX_CAPACITY_SLT_CARD = 8 # This is a capacity for SLIB8 and SLIU8
 
-#
+# Section Card untuk CO-Line (Trunk Slot)
+MAX_CAPACITY_CO_CARD = 4 # This is a capacity for COIU4
+MAX_CO_CARD = 2 # Maximum number for COIU4
+
